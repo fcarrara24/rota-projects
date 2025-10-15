884f3bc467e14b999731353e297faeb3705e735f
fcarrara24 <francescocarrara298@gmail.com>
2025-10-15 22:03:50 +0200

    aggiunta immagini e test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -27,7 +27,7 @@ const About = () => {
           <div className="relative">
             <div className="border border-white overflow-hidden h-[400px] md:h-[500px]">
               <img
-                src="https://images.pexels.com/photos/1267698/pexels-photo-1267698.jpeg?auto=compress&cs=tinysrgb&w=800"
+                src="../img/PRESENTAZIONE_FOTO.jpg"
                 alt="Lavorazioni meccaniche"
                 className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
               />
